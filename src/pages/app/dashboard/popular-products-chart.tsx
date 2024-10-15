e17f824d312b15b5425ca12f33a135c2fa0d726b
alisonbessa <alisonbessa@gmail.com>
2024-10-15 11:15:38 -0300

    feat: create dashboard page and charts

---
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -0,0 +1,92 @@
+import { BarChart } from 'lucide-react'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const data = [
+  { product: 'Pepperoni', amount: 40 },
+  { product: 'Muçaarela', amount: 32 },
+  { product: 'Calabresa', amount: 23 },
+  { product: 'Portuguesa', amount: 16 },
+  { product: 'Frango', amount: 13 },
+]
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+]
+
+export function PopularProductsChart() {
+  return (
+    <Card className="col-span-3">
+      <CardHeader className="pb-8">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-base font-medium">
+            Produtos populares
+          </CardTitle>
+          <BarChart className="h-4 w-4 text-muted-foreground" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={84}
+              innerRadius={48}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat('...')
+                      : data[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, idx) => {
+                return (
+                  <Cell
+                    key={`cell-${idx}`}
+                    fill={COLORS[idx]}
+                    className="stroke-background hover:opacity-80"
+                  />
+                )
+              })}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,89 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  {
+    date: '10/12',
+    revenue: 1200,
+  },
+  {
+    date: '11/12',
+    revenue: 1180,
+  },
+  {
+    date: '12/12',
+    revenue: 2300,
+  },
+  {
+    date: '13/12',
+    revenue: 1600,
+  },
+  {
+    date: '14/12',
+    revenue: 1650,
+  },
+  {
+    date: '15/12',
+    revenue: 1456,
+  },
+  {
+    date: '16/12',
+    revenue: 1123,
+  },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
